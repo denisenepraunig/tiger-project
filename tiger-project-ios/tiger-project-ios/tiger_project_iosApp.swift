0ce5db14c0d2ded435210557c1f87e683dfadf1e
Denise Nepraunig <denise.nepraunig@gmail.com>
2023-07-13 10:54:33 +0200

    start of project

---
+++ tiger-project-ios/tiger-project-ios/tiger_project_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  tiger_project_iosApp.swift
+//  tiger-project-ios
+//
+//  Created by Denise Nepraunig on 13.07.23.
+//
+
+import SwiftUI
+
+@main
+struct tiger_project_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
